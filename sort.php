181057a32633a37d50b361afa97a56d6c884fe31
QueenWang <wangkun011@gmail.com>
2017-05-14 18:42:00 +0800

    update merge sort

--- sort.php
+++ sort.php
@@ -1,4 +1,57 @@
 <?php
 //run this in MAC since PHP has been pre-installed
 //use version 5.6
-echo "hello world \n";
+
+/*
+ * == merge sort ==
+ */
+$arr = array( 6,1,3,7,5,2,3,4,45,5,4,75,8,6,78,7980890,2,4,2,432,5,34,5634,34,5); 
+echo "\n".implode(',',$arr)."\n";
+$arr=mergesort($arr);
+echo "result:".implode(',',$arr)."\n";
+
+//input $numArr
+//output $sortedArr
+//use merge sort using recursion to devide to small similar problem
+//complexity: time n*log(n)
+function mergesort ($numArr) {
+    if (count($numArr) <=1 ) {
+        return $numArr;
+    }
+    else {
+        $midIndex = floor(count($numArr) / 2);
+        $left = array_slice($numArr, 0, $midIndex);
+        $right = array_slice($numArr, $midIndex);
+        
+        $sortedLeft = mergesort($left);
+        $sortedRight = mergesort($right);
+        
+        return merge($sortedLeft, $sortedRight);
+    }
+}
+
+function merge ($sortedLeft, $sortedRight) {
+    $result = array();
+    $i=0;
+    $j=0;
+    while ($i<count($sortedLeft) && $j<count($sortedRight)) {
+        if ($sortedLeft[$i] <= $sortedRight[$j]) {
+            $result[] = $sortedLeft[$i];
+            $i++;
+        }
+        else {
+            $result[] = $sortedRight[$j];
+            $j++;
+        }
+    }
+    //append leftover
+    while($i < count($sortedLeft)) {
+        $result[] = $sortedLeft[$i];
+        $i++;
+    }
+    while ($j < count($sortedRight)) {
+        $result[] = $sortedRight[$j];
+        $j++;
+    }
+    return $result;
+}
--- sort_mergesort_online.php
+++ sort_mergesort_online.php
@@ -1,4 +1,61 @@
 <?php
 //run this in MAC since PHP has been pre-installed
 //use version 5.6
-echo "hello world \n";
+
+//merge sort
+$arr = array( 6,1,3,7,5,2,3,4,45,5,4,75,8,6,78,7980890,2,4,2,432,5,34,5634,34,5); 
+echo "\n".implode(',',$arr)."\n";
+$arr=mergesort($arr);
+echo implode(',',$arr);
+
+//copy online
+function mergesort($numlist)
+{
+    echo "mergesort: ".implode(',',$numlist)."\n";
+    if(count($numlist) == 1 ) return $numlist;
+ 
+    $mid = count($numlist) / 2;
+    $left = array_slice($numlist, 0, $mid);
+    $right = array_slice($numlist, $mid);
+    
+    echo "call mergesort(left), caller pause, callee active \n";
+    $left = mergesort($left);
+    echo "call mergesort right), caller pause, callee active \n";
+    $right = mergesort($right);
+     echo "call merge \n";
+    return merge($left, $right);
+}
+ 
+function merge($left, $right)
+{
+    echo "merge ".implode(',',$left).", right=".implode(',',$right)."\n";
+    $result=array();
+    $leftIndex=0;
+    $rightIndex=0;
+ 
+    while($leftIndex<count($left) && $rightIndex<count($right))
+    {
+        if($left[$leftIndex]>$right[$rightIndex])
+        {
+ 
+            $result[]=$right[$rightIndex];
+            $rightIndex++;
+        }
+        else
+        {
+            $result[]=$left[$leftIndex];
+            $leftIndex++;
+        }
+    }
+    while($leftIndex<count($left))
+    {
+        $result[]=$left[$leftIndex];
+        $leftIndex++;
+    }
+    while($rightIndex<count($right))
+    {
+        $result[]=$right[$rightIndex];
+        $rightIndex++;
+    }
+    return $result;
+}
