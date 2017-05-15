a10ffff2bdd9bf5f890e11bbf6fc8d50d2efbe0b
QueenWang <wangkun011@gmail.com>
2017-05-15 10:36:24 +0800

    array.php for array related code

--- array.php
+++ array.php
@@ -1,4 +1,9 @@
 <?php
 //run this in MAC since PHP has been pre-installed
 //use version 5.6
-echo "hello world \n";
+
+
+//Given two arrays, return an array that contains all elements that are in the intersection of both
+
+//input: $array1, $array2
+//output: $intersectArr
