d7aa8e83501a5e93de667166833b370849d38b79
wangkun <wangkun@localhost>
2022-12-06 11:34:14 +0800

    add sequential search

--- 1_binarysearch.php
+++ 1_binarysearch.php
@@ -14,19 +14,37 @@ $index
  */
 //http://www.geeksforgeeks.org/find-first-last-occurrences-element-sorted-array/
 
-$sortedNumArr = [4, 4, 4, 4, 4, 6, 6, 7, 7, 7, 8, 9, 10];
-echo "sorted list of numbers: ".implode(",", $sortedNumArr)."\n";
 $searchVal = (int) $argv[1];
+$sortedNumArr = [4, 4, 4, 4, 4, 6, 6, 7, 7, 7, 8, 9, 10];
+$sortNumArr2 = array(0, 1, 2, 3, 4, 5, 6);
+$unsortedNumArr = [7, 4, 3, 10, 9, 8, 6, 4, 4, 4, 4, 6, 6, 7, 7, 7, 8, 9, 10];
+
+echo "\n***** binary search - first/last occurance *****\n";
+echo "sorted list of numbers: ".implode(",", $sortedNumArr)."\n";
 $result = getFirstIndex($sortedNumArr, $searchVal, 0, count($sortedNumArr)-1);
 echo "firt occurance result: $result \n";
 $result = getLastIndex($sortedNumArr, $searchVal, count($sortedNumArr), 0, count($sortedNumArr)-1);
 echo "last occurance result: $result \n";
 
 //test binary search
-$sortNumArr = array(0, 1, 2, 3, 4, 5, 6);
-echo "\nsorted list of numbers: ".implode(",", $sortNumArr)."\n";
-$result = binarySearch($sortNumArr, $searchVal, 0, count($sortNumArr)-1);
+echo "\n***** binary search *****\n";
+echo "sorted list of numbers: ".implode(",", $sortNumArr2)."\n";
+$result = binarySearch($sortNumArr2, $searchVal, 0, count($sortNumArr2)-1);
 echo "binary result: $result \n";
+
+//test sequential search
+echo "\n***** sequential search sorted *****\n";
+echo "sorted list of numbers: ".implode(",", $sortedNumArr)."\n";
+$resultArr = sequentialSearchFirstLast($sortedNumArr, $searchVal);
+echo "firt occurance result: $resultArr[0] \n";
+echo "last occurance result: $resultArr[1] \n";
+
+//test sequential search
+echo "\n***** sequential search unsorted *****\n";
+echo "sorted list of numbers: ".implode(",", $unsortedNumArr)."\n";
+$resultArr = sequentialSearchUnsorted($unsortedNumArr, $searchVal);
+echo "firt occurance result: $resultArr[0] \n";
+echo "last occurance result: $resultArr[1] \n";
 
 //first occurance
 function getFirstIndex($sortedNumArr, $searchVal, $searchStartIndex, $searchEndIndex) {
@@ -98,4 +116,44 @@ function binarySearch ($sortNumArr, $givenNum, $searchStartIndex, $searchEndInde
     else {
         return binarySearch($sortNumArr, $givenNum, $midIndex+1, $searchEndIndex);
     }
+}
+
+//native thinking to search a sorted array: loop from first item to last item
+//time complexisity is O(n)
+//find first and last occurance both, return index
+//another name is linear search
+function sequentialSearchFirstLast ($sortNumArr, $givenNum) {
+    $arrayCount = count($sortNumArr);
+    $firstIndex = -1;
+    $lastIndex = -1;
+    for ($i = 0; $i < $arrayCount; $i++) {
+        if ($sortNumArr[$i] > $givenNum ) { //if number is big than search number, stop checking
+            break;
+        }
+        else if ($sortNumArr[$i] == $givenNum) {
+            if ($firstIndex == -1) {
+                $firstIndex = $i;
+            }
+            $lastIndex = $i;
+        }
+    }
+    
+    return [$firstIndex, $lastIndex];
+}
+
+//same as sorted, only difference is this must loop through whole array
+function sequentialSearchUnsorted ($unsortNumArr, $givenNum) {
+    $arrayCount = count($unsortNumArr);
+    $firstIndex = -1;
+    $lastIndex = -1;
+    for ($i = 0; $i < $arrayCount; $i++) {
+        if ($unsortNumArr[$i] == $givenNum) {
+            if ($firstIndex == -1) {
+                $firstIndex = $i;
+            }
+            $lastIndex = $i;
+        }
+    }
+    
+    return [$firstIndex, $lastIndex];
 }
