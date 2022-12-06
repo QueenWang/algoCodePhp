f9c56a19d7cc44304462dfcd8e5159da219b7019
wangkun <wangkun@localhost>
2022-12-06 12:01:36 +0800

    rename file

--- practice_2022dec/1_search_2_firstlast.php
+++ practice_2022dec/1_search_2_firstlast.php
@@ -9,7 +9,11 @@
 
 //problem 2: https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
 //34. Find First and Last Position of Element in Sorted Array
-//Kun note: one function for starting, one function for ending
+/*
+ * Kun note: 
+ * - one function for starting, one function for ending
+ * - having duplicate numbers
+ */
 
 class Solution {
 
